6fcb17fa20dff3b639a6241ec79bf722438f2c4e
Adrian Cooney <cooney.adrian@gmail.com>
2013-07-06 00:03:19 +0100

    Switching to master to add more tests. It's getting messy switching to and fro

--- script/function.js
+++ script/function.js
@@ -1,33 +1,34 @@
-var timeout;
-Voyeur.find("#input").addEventListener("keydown", function() {
-	if(timeout) clearTimeout(timeout);
-	timeout = setTimeout(runExample, 1000);
-});
-
 function runExample() {
-	console.log(Voyeur.find("#hidden-doc"));
 	if(Voyeur.find("#hidden-doc")) Voyeur.find("#hidden-doc").parentNode.removeChild(Voyeur.find("#hidden-doc"));
 	Voyeur.find(".output").create.iframe.use(function(iframe) {
 		iframe.id = "hidden-doc";
 		iframe.style.display = "none";
 
+		var text = Voyeur.find("#input").textContent;
+
 		//Slightly embarrassing that I can't use Voyeur here
 		var doc = iframe.contentWindow.document;
 
 		var script = doc.createElement("script"),
-			voyeurjs = doc.createElement("script");
-
-		voyeurjs.src = "../Voyeur.js";
+			externals = ["Voyeur.js", "../HTML.stringify/HTML.stringify.js"].map(function(val) { return "<script type='text/javascript' src='" + val + "'></script>"; }).join("");
 
 		//Append the code
-		script.textContent = "document.addEventListener('DOMContextLoaded', function() { " + text + " });";
+		script.textContent = "window.addEventListener('DOMContentLoaded', function() { console.log('IFRAME:',  Voyeur); });"
 
 		//Append the script
-		doc.appendChild(voyeurjs);
-		doc.appendChild(script);
-
-		setTimeout(function() {
-
-		}, 100)
+		doc.body.innerHTML = externals;
+		doc.body.appendChild(script);
 	});
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	//The intent edit
+	var timeout;
+	Voyeur.find("#input").addEventListener("keydown", function() {
+		if(timeout) clearTimeout(timeout);
+		timeout = setTimeout(runExample, 1000);
+	});
+
+	//Initially run the example
+	runExample();
+})
